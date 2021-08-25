879bc0780d0b84e4a1d8e65defe0587492234c23
Thyago Porto <thyago.cabralporto@gmail.com>
2021-08-25 03:15:40 -0300

    Primeiro commit

---
+++ src/AppContext/Provider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppContext from  './Context';
+
+const AppProvider  =  ({ children }) => {
+    return (
+        <AppContext.Provider value={{ user : 'DEV'}}>
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export default AppProvider;
